2448acaa123a62f434c5aa48006f5e3eedc2c02a
Sekh <sekh-mahadi>
2022-01-08 22:40:13 +0600

    Project Lombok with Maven

---
+++ Project-Lombok/src/main/java/io/mtech/User.java
@@ -0,0 +1,16 @@
+package io.mtech;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    private String firstName;
+    private String lastName;
+    private String email;
+}
