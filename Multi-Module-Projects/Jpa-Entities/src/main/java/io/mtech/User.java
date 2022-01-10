e4ee1cf7f940e5745559ab2e89bdf4f629dd19a9
Sekh <sekh-mahadi>
2022-01-10 19:50:24 +0600

    Created Multi-Module Project With Maven.

---
+++ Multi-Module-Projects/Jpa-Entities/src/main/java/io/mtech/User.java
@@ -0,0 +1,22 @@
+package io.mtech;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@Entity
+public class User {
+    @Id
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+}
---
+++ Multi-Module-Projects/from-intellij/src/main/java/io/mtech/MainTest.java
@@ -0,0 +1,7 @@
+package io.mtech;
+
+public class MainTest {
+    public String sayHello(){
+        return "Hello Multu-Module";
+    }
+}
